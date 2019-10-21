912660a027281a949b59ce7166d4f18c3ac2f7cb
aindriu <aindriu80@gmail.com>
2019-10-21 20:15:13 +0100

    Next.js Components & Pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import User from '../../components/User';
 
 const authIndexPage = () =>(
     <div>
         <h1>The Auth Index page</h1>
+        <User name="Aindriú" age={39} />
     </div>
 );
 
